d74bba7445deb1a77a70a60c8a660155cfae8135
Kevin Brasier <wonderfulweb@gmail.com>
2015-10-21 00:37:00 -0700

    Minor Tweaks, manifest, etc

--- app/src/main/java/com/blueheronsresistance/stattracker/MyVolleySingleton.java
+++ app/src/main/java/com/blueheronsresistance/stattracker/MyVolleySingleton.java
@@ -27,7 +27,7 @@ public class MyVolleySingleton {
         return mInstance;
     }
 
-    public RequestQueue getRequestQueue() {
+    private RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
             // getApplicationContext() is key, it keeps you from leaking the
             // Activity or BroadcastReceiver if someone passes one in.
--- app/src/main/java/com/blueheronsresistance/stattracker/SettingsManualInputDialogFragment.java
+++ app/src/main/java/com/blueheronsresistance/stattracker/SettingsManualInputDialogFragment.java
@@ -1,16 +1,18 @@
 package com.blueheronsresistance.stattracker;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.widget.EditText;
 
 /**
- * Created by kevin on 9/23/15.
+ * Dialog for taking in a Stat Tracker token manually
  */
 public class SettingsManualInputDialogFragment extends DialogFragment {
 
@@ -21,7 +23,7 @@ public class SettingsManualInputDialogFragment extends DialogFragment {
     }
 
     // Use this instance of the interface to deliver action events
-    SettingsManualInputDialogListener mListener;
+    private SettingsManualInputDialogListener mListener;
 
     // Override the Fragment.onAttach() method to instantiate the SettingsManualInputDialogListener
     @Override
@@ -38,9 +40,11 @@ public class SettingsManualInputDialogFragment extends DialogFragment {
         }
     }
 
-    @Override
+    @SuppressLint("InflateParams")
+    @Override @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
@@ -48,15 +52,15 @@ public class SettingsManualInputDialogFragment extends DialogFragment {
         builder.setTitle(R.string.settings_manual_input_dialog_title)
                 .setView(inflater.inflate(R.layout.dialog_settings_manual_input, null))
                 .setPositiveButton(R.string.settings_manual_input_dialog_ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // Send the positive button event back to the host activity
+            public void onClick(DialogInterface dialog, int id) {
+                // Send the positive button event back to the host activity
 
-                        String tokenName = ((EditText) getDialog().findViewById(R.id.settings_manual_input_dialog_name)).getText().toString();
-                        String token = ((EditText) getDialog().findViewById(R.id.settings_manual_input_dialog_value)).getText().toString();
-                        String issuerUrl = ((EditText) getDialog().findViewById(R.id.settings_manual_input_dialog_url)).getText().toString();
-                        mListener.onSettingsManualInputDialogOkClick(tokenName, token, issuerUrl);
-                    }
-                })
+                String tokenName = ((EditText) getDialog().findViewById(R.id.settings_manual_input_dialog_name)).getText().toString();
+                String token = ((EditText) getDialog().findViewById(R.id.settings_manual_input_dialog_value)).getText().toString();
+                String issuerUrl = ((EditText) getDialog().findViewById(R.id.settings_manual_input_dialog_url)).getText().toString();
+                mListener.onSettingsManualInputDialogOkClick(tokenName, token, issuerUrl);
+            }
+        })
                 .setNegativeButton(R.string.settings_manual_input_dialog_cancel, null);
 
         // Create the AlertDialog object and return it
